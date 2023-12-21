e21d4510db4c8723b4a89811c509a83588262854
tsyganiimaksym <tsyganiimaksym@gmail.com>
2023-12-21 22:52:15 +0100

    gotovo

--- app/src/main/java/com/myapp/lab5/MainActivity.kt
+++ app/src/main/java/com/myapp/lab5/MainActivity.kt
@@ -7,12 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.myapp.lab5.ui.theme.Lab5Theme
 
